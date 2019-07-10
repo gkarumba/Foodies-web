7b01071cd675c9413b6273fb5772cdcdf08feade
DMG <dmwaigithinji@gmail.com>
2019-07-10 05:30:13 +0300

    Add restaurant card

--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from '../../restaurants/restaurant.model';
 
-}
+
 @Component({
   selector: 'restaurant-card',
   templateUrl: './restaurant-card.component.html',
   styleUrls: ['./restaurant-card.component.css']
 })
 
+export class RestaurantCardComponent {
+  restaurants = [
+    new Restaurant(1, 'Java','CBD', 'https://www.celadonnapa.com/wp-content/uploads/2015/02/celadon-food.jpg'),
+    new Restaurant(2, 'Urban Gormet', "Ngong' Road", 'https://www.celadonnapa.com/wp-content/uploads/2015/02/celadon-food.jpg'),
+    new Restaurant(3, 'KFC', 'CBD', 'https://www.celadonnapa.com/wp-content/uploads/2015/02/celadon-food.jpg'),
+    new Restaurant(4, 'Wimpy', 'Upperhill', 'https://www.celadonnapa.com/wp-content/uploads/2015/02/celadon-food.jpg'),
+    new Restaurant(5, "Moody's Restaurant", 'Upperhill', 'https://www.celadonnapa.com/wp-content/uploads/2015/02/celadon-food.jpg')
+    ]
 
-
-export class QuoteComponent implements OnInit {
-  quotes = [
-    new Restaurant(1, 'Java','CBD', 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015'),
-    new Restaurant(2, 'Urban Gormet', "Ngong' Road", 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015'),
-    new Restaurant(3, 'KFC', 'CBD', 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015'),
-    new Restaurant(4, 'Wimpy', 'Upperhill', 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015'),
-    new Restaurant(5, "Moody's Restaurant", 'Upperhill', 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015'),
-    new Restaurant(6, "Kaldhi'S Restaurant", "Ngong' Road", 'https://d1ralsognjng37.cloudfront.net/d5a447f5-87bc-453e-89a6-a137474e0015')
-  ]
-  ngOnInit() {}
 
 }
